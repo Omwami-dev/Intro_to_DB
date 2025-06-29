63426ed7ee2ca2e840cef763b9fe8c61ff576285
Omwami-dev <barasaowabarasa@gmail.com>
2025-06-29 09:44:33 +0300

    made changes on alx_book_stored.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 CREATE TABLE Authors(author_id AUTO_INCREMENT  PRIMARY KEY, author_name VARCHAR(215) NOT NULL);
 CREATE TABLE Books(book_id AUTO_INCREMENT PRIMARY KEY, title VARCHAR(130) NOT NULL, author_id NOT NULL,price DOUBLE NOT NULL, publication_date DATE
